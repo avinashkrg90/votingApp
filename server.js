c157cc89ca9aa91b4da278d3deb4c5fffb846b0d
Avinash Kumar Gupta <robozealous@gmail.com>
2024-08-30 15:13:09 +0530

    a line added for testing

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
+app.get('/', (req, res)=>{
+    res.send("Hi, welcome to my server!!")
+})
+
 app.listen(PORT, ()=>{
     console.log(`server listening on port ${PORT}`)
 })
