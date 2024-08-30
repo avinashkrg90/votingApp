0aa88bd6be83930dee3bf35d560e4386725b5db8
Avinash Kumar Gupta <robozealous@gmail.com>
2024-08-30 16:38:52 +0530

    variable being passed to the template engine

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express')
 
 const app = express()
-
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res)=>{
-    res.render("index")
+    res.render("index", {text: "world"})
 })
 
 app.listen(PORT, ()=>{
